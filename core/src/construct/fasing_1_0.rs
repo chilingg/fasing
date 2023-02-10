5753ab37009fb4f13ba2ee7e171951881ac9fd79
chilingg <chilingg@qq.com>
2023-02-11 03:35:02 +0800

    添加结构表完整性测试

--- core/src/construct/fasing_1_0.rs
+++ core/src/construct/fasing_1_0.rs
@@ -10,3 +10,26 @@ pub fn generate_table() -> Table {
 pub fn generate_fas_file() -> FasFile {
     serde_json::from_str(include_str!("../../tmp/fasing_1_0.fas")).unwrap()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_completeness() {
+        let table = generate_table();
+        let requests = all_requirements(&table);
+
+        let mut misses = std::collections::HashSet::new();
+
+        requests.into_iter().for_each(|name| {
+            let mut chars = name.chars();
+            let chr = chars.next().unwrap();
+            if chars.next().is_none() && !table.contains_key(&chr) {
+                misses.insert(chr);
+            }
+        });
+
+        assert_eq!(misses, std::collections::HashSet::new());
+    }
+}
