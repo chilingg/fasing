03b21f40de2b0f630f292c5819791c02f898eb71
chilingg <chilingg@qq.com>
2023-02-21 15:32:49 +0800

    修改消息面板顺序显示

--- editor/src/widgets/message.rs
+++ editor/src/widgets/message.rs
@@ -7,42 +7,34 @@ pub enum MsgType {
 
 pub struct MessagePanel {
     y_offset: f32,
-    messages: Option<Vec<MsgType>>,
+    messages: Vec<MsgType>,
 }
 
 impl Default for MessagePanel {
     fn default() -> Self {
         Self {
             y_offset: 0.0,
-            messages: Some(vec![]),
+            messages: vec![],
         }
     }
 }
 
 impl MessagePanel {
     pub fn add_info<T: Into<String>>(&mut self, msg: T) {
-        self.messages
-            .as_mut()
-            .unwrap()
-            .push(MsgType::Info(msg.into()));
+        self.messages.push(MsgType::Info(msg.into()));
     }
 
     pub fn add_warning<T: Into<String>>(&mut self, msg: T) {
-        self.messages
-            .as_mut()
-            .unwrap()
-            .push(MsgType::Warning(msg.into()));
+        self.messages.push(MsgType::Warning(msg.into()));
     }
 
     pub fn add_error<T: Into<String>>(&mut self, msg: T) {
-        self.messages
-            .as_mut()
-            .unwrap()
-            .push(MsgType::Error(msg.into()));
+        self.messages.push(MsgType::Error(msg.into()));
     }
 
     pub fn update(&mut self, ctx: &egui::Context) {
         const PANEL_WIDTH: f32 = 240.0;
+        const OUT_MARGIN: f32 = 10.0;
 
         let rect = ctx.screen_rect();
 
@@ -53,47 +45,32 @@ impl MessagePanel {
             ))
             .show(ctx, |ui| {
                 let mut height = 0.0;
-                ui.allocate_ui_with_layout(
-                    egui::Vec2::new(PANEL_WIDTH, ui.available_height()),
-                    egui::Layout::bottom_up(egui::Align::LEFT),
-                    |ui| {
-                        self.messages = Some(
-                            self.messages
-                                .take()
-                                .unwrap()
-                                .into_iter()
-                                .filter(|msg| {
-                                    let response = egui::Frame::none()
-                                        .fill(ui.style().visuals.panel_fill)
-                                        .inner_margin(egui::Margin::symmetric(12.0, 12.0))
-                                        .outer_margin(egui::Margin {
-                                            bottom: 10.0,
-                                            ..Default::default()
-                                        })
-                                        .shadow(ui.style().visuals.window_shadow)
-                                        .show(ui, |ui| {
-                                            ui.set_min_width(PANEL_WIDTH);
-                                            match msg {
-                                                MsgType::Info(msg) => ui.label(msg),
-                                                MsgType::Warning(msg) => ui.colored_label(
-                                                    ui.style().visuals.warn_fg_color,
-                                                    msg,
-                                                ),
-                                                MsgType::Error(msg) => ui.colored_label(
-                                                    ui.style().visuals.error_fg_color,
-                                                    msg,
-                                                ),
-                                            }
-                                        })
-                                        .response;
+                self.messages.retain(|msg| {
+                    let response = egui::Frame::none()
+                        .fill(ui.style().visuals.panel_fill)
+                        .inner_margin(egui::Margin::symmetric(12.0, 12.0))
+                        .outer_margin(egui::Margin {
+                            bottom: OUT_MARGIN,
+                            ..Default::default()
+                        })
+                        .shadow(ui.style().visuals.window_shadow)
+                        .show(ui, |ui| {
+                            ui.set_min_width(PANEL_WIDTH);
+                            match msg {
+                                MsgType::Info(msg) => ui.label(msg),
+                                MsgType::Warning(msg) => {
+                                    ui.colored_label(ui.style().visuals.warn_fg_color, msg)
+                                }
+                                MsgType::Error(msg) => {
+                                    ui.colored_label(ui.style().visuals.error_fg_color, msg)
+                                }
+                            }
+                        })
+                        .response;
 
-                                    height += response.rect.height();
-                                    !response.hovered()
-                                })
-                                .collect(),
-                        )
-                    },
-                );
+                    height += response.rect.height() + 3.0;
+                    !response.hovered()
+                });
 
                 if self.y_offset < height {
                     self.y_offset += 4.0;
